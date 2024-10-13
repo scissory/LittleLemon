ba9eb571b99afd716baddcfa8c29c8126764060d
David Lloyd <scissory@yahoo.com>
2024-10-13 10:20:51 -0700

    Booking form working, now with unit tests

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,7 +1,14 @@
-const ConfirmedBooking = () => { 
-    return (
-        <div>stub</div>
-    )
-}
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const ConfirmedBooking = () => {
+  return (
+    <div className="container confirmed-booking">
+      <FontAwesomeIcon icon={faCircleCheck} size="3x" />
+      <h2>Your reservation has been confirmed.</h2>
+      <p>You will receive an email with all the details.</p>
+    </div>
+  );
+};
 
 export default ConfirmedBooking;
